767ba925b3f9a7fa7569ef45a411b908e8920623
fountesh <128893715+fountesh@users.noreply.github.com>
2023-08-28 01:05:43 +0300

    update 1
    Add some new function

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
-bot_token = "token"
+bot_token = "6346817681:AAGaRprnqrN17gygtgUV7BGeJsXfl9TIBS4"
 
 import telebot
+import time
 from telebot import types
 from notifiers import get_notifier
 
-quea_list = ["first", "second", "third"]
+quea_list = ["ти і я", "він і вона", "воно і Даша"]
 global i
 i = 0
 
@@ -12,21 +13,40 @@ bot = telebot.TeleBot(bot_token)
 
 @bot.message_handler(commands=['start'])
 def start_function(message):
-    bot.send_message(message.chat.id, "Привіт! \nЯ бот, який створений для того, щоб ви знали, хто чергує сьогодні")
+    bot.send_message(message.chat.id, "Привіт! \nЯ бот, який створений для того, щоб ви знали, хто чергує сьогодні\nДля того щоб ввімкнути кнопки напишіть /work")
 
 @bot.message_handler(commands=['work'])
 def work(message):
-    markup = types.ReplyKeyboardMarkup()
-    who = types.KeyboardButton("Хто черговий?")
-    markup.add(who)
-    bot.send_message(message.chat.id, "Працюю", reply_markup=markup)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    who = types.KeyboardButton("Обрати чергових")
+    see = types.KeyboardButton("Дізнатися хто черговий")
+    markup.add(who, see)
+    bot.send_message(message.chat.id, "Працюю", reply_markup=markup,)
 
 @bot.message_handler(content_types="text")
 def react_on_message(message):
     global i
-    if message.text == "Хто черговий?":
-        bot.send_message(message.chat.id, "Сьогодні чергові:\n" + quea_list[i])
-        i += 1
-        if i >= 3:
-            i = 0   
+     
+    if message.text == "Обрати чергових":
+        markup_inline = types.InlineKeyboardMarkup()
+        aggre = types.InlineKeyboardButton("✅", callback_data="Виходить")
+        disagre = types.InlineKeyboardButton("❌", callback_data="Не виходить")
+        markup_inline.add(aggre, disagre)
+        bot.send_message(message.chat.id, "Сьогодні мають чергувати:\n" + quea_list[i] + "\nУ вас виходить?", reply_markup=markup_inline)
+    if message.text == "Дізнатися хто черговий":
+        bot.send_message(message.chat.id, "Сьогодні чергові:\n" + quea_list[i - 1])
+    @bot.callback_query_handler(func=lambda call:True)
+    def answer(call):
+        global i
+        if call.data == "Виходить":
+            bot.send_message(message.chat.id, "Чергові:\n" + quea_list[i])
+            i += 1
+            if i >= 3:
+                i = 0
+        if call.data == "Не виходить":
+            i += 1
+            if i >= 3:
+                i = 0
+            bot.send_message(message.chat.id, "Тоді сьогодні чергові:\n" + quea_list[i], reply_markup=markup_inline)
+
 bot.infinity_polling(none_stop= True)
